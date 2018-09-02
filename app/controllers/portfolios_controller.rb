638fa9848140d6f60b7b64c3b4a0d744204f0f06
Natalie Lynch <Natalie@Natalies-MacBook-Pro.local>
2018-09-02 07:41:54 -0500

    implimented destroy action for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,4 +40,17 @@ def show
   @portfolio_item = Portfolio.find(params[:id])
 end
 
+def destroy
+  # THis is going to perform the lookup
+  @portfolio_item = Portfolio.find(params[:id])
+
+  @portfolio_item.destroy
+
+  respond_to do |format|
+    format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
+  end
+  
+end
+
+
 end
